eb95251d9158aa18ad97180fea1c2af1fee2d57a
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-07-13 11:36:21 +0530

    Pointers With Arrays

--- Pointers in cpp/pointersWithArrays3.cpp
+++ Pointers in cpp/pointersWithArrays3.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
     int arr[10]={7,3,5,9};
     int *p= &arr[0];
-    cout<<p<<endl;
-    cout<<&p<<endl;
+    cout<<p<<endl;  //prints address of arr;
+    cout<<&p<<endl; //prints address of pointer;
+    
+
 
 
     return 0;
