34249b27e090f770c6d7c1245d14398cd5a5e57a
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-07-16 14:27:32 +0530

    PA

---
+++ Book questions/char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+float print(){
+  int a=10.345876643;
+  return a;
+}
+int main(){ //to print ascii value of a character we will do explicit typecasting using int
+    int b=10.6476574675638758;
+  cout<<b;
+    cout<<print(); 
+
+    return 0;
+}
--- Macros/practice.cpp
+++ Macros/practice.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include<iostream> //preprocessor directive statement
 using namespace std;
-#define LIMIT 5
+#define LIMIT 5 //preprocessor directive //cannot be modified this means it is a constant value
 int main(){
     for (int i = 0; i < LIMIT; i++)
     {
         /* code */
         cout<<i;
     }
+    LIMIT=10;
+    cout<<LIMIT;
     
 
     
