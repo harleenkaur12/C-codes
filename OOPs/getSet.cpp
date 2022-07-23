c9ceef77b322e1aa284a83b26b1a7a5a1550f6b5
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-07-23 16:05:53 +0530

    PA

---
+++ OOPs/getSet.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class hero{
+    private:
+    int health;
+    
+    public:
+    char level;
+
+    void print(){
+        cout<<level<<endl;
+    }
+
+    int getHealth(){
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+
+    void setHealth(int h){
+        health=h;
+    }
+
+};
+
+int main(){
+    hero Ramesh;
+    cout<<"Health of Ramesh is "<<Ramesh.getHealth()<<endl;  //returns some garbage value
+    Ramesh.setHealth(70);
+    cout<<"Health of Ramesh is "<<Ramesh.getHealth();
+
+}
