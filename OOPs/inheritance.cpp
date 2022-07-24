af57342fd77a74a3ba8fd419f293251e2dcdfe09
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-07-24 19:55:44 +0530

    PA

---
+++ OOPs/inheritance.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class human{
+    public:
+    int height;
+    int weight;
+    int age;
+
+    public:
+    int getAge(){
+        return this->age;
+    }
+    int setWeight(int w){
+        this->weight=w;
+    }
+};
+class male: public human {
+    public:
+    string color;
+
+    void sleep(){
+        cout<< "Male sleeping "<<endl;
+    }
+};
+
+int main(){
+
+    male object1;
+    cout<<object1.age<<endl;
+    cout<<object1.color<<endl;
+    object1.setWeight(88);
+    cout<<object1.weight<<endl;
+    object1.sleep();
+    
+    return 0;
+}
