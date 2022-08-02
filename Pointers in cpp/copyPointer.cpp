853926d6452a445c28d1abeabd09b21bff89742d
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-08-02 21:39:12 +0530

    .

---
+++ Pointers in cpp/copyPointer.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int *p=&a;
+    int b=20;
+    int *q=&b;
+    
+    return 0;
+}
---
+++ Pointers in cpp/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+int *q=&b;
+    // cout<<"Address of a is "<<p<<endl;
+    // cout<<"Address of b is "<<q<<endl;
