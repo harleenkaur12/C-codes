4a279e3718de3d7fcebe1201fde9a5c06b91fa65
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-08-02 21:42:01 +0530

    .

--- Pointers in cpp/copyPointer.cpp
+++ Pointers in cpp/copyPointer.cpp
@@ -5,6 +5,16 @@ int main(){
     int *p=&a;
     int b=20;
     int *q=&b;
-    
+    p=q; //as they are already initialised
+    cout<<"Address of p is "<<p<<endl;
+    cout<<"Address of q is "<<q<<endl;
+
+
+    int x=10;
+    int *l=&x;
+    int *m=l;
+    cout<<"Address of l is "<<l<<endl;
+    cout<<"Address of m is "<<m<<endl;
+
     return 0;
 }
