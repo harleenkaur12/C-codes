a0ea7910d52b6c347c7a1d39a5a1faef4403f42a
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-08-05 09:45:52 +0530

    .

---
+++ CPP_Classroom/OOPs/Inheritance/practice ques/practice.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class employee{
+    int empNo;
+    string empName;
+    int years;
+    int punctual;
+    int taskComp;
+    int onTimeSub;
+    
+    
+    int calculatetotal(){
+        int totalGrade=punctual+taskComp+onTimeSub;
+        return totalGrade;
+        
+    }
+    int calculatePercent(){
+        int totalGrade=punctual+taskComp+onTimeSub;
+        int percentage=totalGrade/3;
+        return percentage;
+    }
+
+    public:
+    employee(){};
+
+    void takedata(int n, string name, int y,int p, int tc, int ots){
+        empNo=n;
+        empName=name;
+        years=y;
+        punctual=p;
+        taskComp=tc;
+        onTimeSub=ots;
+    }
+    void showdata(){
+        cout<<empNo<<" "<<empName<<" "<<years<<" "<<punctual<<" "<<taskComp<<" "<<onTimeSub<<endl;
+        cout<<"Total grade is "<<calculatetotal()<<endl;
+        cout<<"Grade percentage is "<<calculatePercent()<<"%"<<endl;
+    }
+};
+int main(){
+    employee e1;
+    e1.takedata(1,"Harsh",5,100,100,100);
+    e1.showdata();
+    
+
+
+    return 0;
+}
