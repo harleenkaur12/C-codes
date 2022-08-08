05700ae0cf8a7cfbc1f321ab144d3ad84ed38bfb
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-08-08 09:32:36 +0530

    .

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    //base case
+   
+    
+    // int smallerProblem=factorial(n-1);
+    // int biggerProblem=n*smallerProblem;
+    // return biggerProblem;
+     if(n==0)
+      return 1;
+    
+    return n*factorial(n-1);
+   
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans=factorial(n);
+    cout<<ans<<endl;
+    return 0;
+}
