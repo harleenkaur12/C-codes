a81ece1994a83bfd7fa2e532bd06b761e92eb3fa
harleenkaur12 <harleen1555.be20@chitkara.edu.in>
2022-08-25 13:03:17 +0530

    .

--- CPP_Classroom/Exception Handling/e6.cpp
+++ CPP_Classroom/Exception Handling/e6.cpp
@@ -4,13 +4,21 @@ template<class T>
 class sample{
     public:
     T a,b;
-    sample(T x=0, T y=0){
-        this->a=x;
-        this->b=y;
-    }
+
+    // sample(T x=0, T y=0){
+    //     this->a=x;
+    //     this->b=y;
+    // }this will not work in case of string and char
+    sample(){};
+    sample(T x, T y){
+        x=a;
+        y=b;
+    };
 };
 
 int main(){
+   
+
 
     return 0;
 }
